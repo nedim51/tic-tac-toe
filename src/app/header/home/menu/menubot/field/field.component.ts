3f8a4282fc4ad1fc44a5f63814e797fe150ef078
unknown <nedim51@mail.ru>
2018-11-14 18:41:56 +0300

    com

--- src/app/header/home/menu/menubot/field/field.component.ts
+++ src/app/header/home/menu/menubot/field/field.component.ts
@@ -12,4 +12,26 @@ export class FieldComponent implements OnInit {
   ngOnInit() {
   }
 
+  content = 'panorama_fish_eye';
+  player = 'cr';
+
+  field = [[{ class: '', content: '' }, { class: '', content: '' }, { class: '', content: '' }],
+           [{ class: '', content: '' }, { class: '', content: '' }, { class: '', content: '' }],
+           [{ class: '', content: '' }, { class: '', content: '' }, { class: '', content: '' }]];
+
+  add(i, j) {
+    // this.message ='';
+    if (!(this.field[i][j].content) && !(this.field[i][j].class)) {
+      if (this.content == 'panorama_fish_eye') {
+        this.player= 'cr';
+        this.content = 'clear';
+      }
+      else if (this.content == 'clear') {
+        this.content = 'panorama_fish_eye';
+        this.player= 'zr';
+      }
+      this.field[i][j].class = this.player;
+      this.field[i][j].content = this.content;
+    }
+  }
 }
